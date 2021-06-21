18911abe4c3330b0d0b888e4586a33baebb7c3a9
Rony Quispe <oghmand@gmail.com>
2021-06-20 22:59:58 -0500

    Guardando un producto.

--- app/Http/Livewire/Product/Create.php
+++ app/Http/Livewire/Product/Create.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Livewire\Product;
 
+use App\Models\Product;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class Create extends Component
 {
-    public $name;
-    
+    use WithFileUploads;
+
+    public $name,$price,$description,$thumbnail;
+
+    public function save(){
+        $validate = $this->validate([
+            'name' => 'required',
+            'price' => 'required|numeric',
+            'description' => 'required',
+            'thumbnail' => 'image|max:1024'
+        ]);
+
+        $validate['thumbnail'] = $this->thumbnail->store('photos');
+        $product = Product::create($validate);
+        return $this->redirect('');
+    }
+
     public function render()
     {
         return view('livewire.product.create');
--- resources/views/livewire/product/create.blade.php
+++ resources/views/livewire/product/create.blade.php
@@ -1,5 +1,29 @@
 <div>
     <h2 class="text-center">Agregar nuevo producto</h2>
-    <p>{{ $name }}</p>
-    <input type="text" wire:model = 'name'>
+    <div class="row justify-content-center">
+        <div class="col-sm-6">
+        <form wire:submit.prevent="save">
+            <label for="">Nombre producto</label>
+            <input type="text" wire:model="name" class="form-control mb-2">
+            @error('name') <p class="error mb-2">{{ $message }}</p> @enderror
+
+            <label for="">Descripción</label>
+            <textarea cols="20" rows="5" wire:model="description" class="form-control mb-2"></textarea>
+            @error('description') <p class="error mb-2">{{ $message }}</p> @enderror
+
+            <label for="">Precio</label>
+            <input type="text" wire:model="price" class="form-control mb-2"> 
+            @error('price') <p class="error mb-2">{{ $message }}</p> @enderror
+
+            <label for="">Imagen</label>
+            <input type="file" wire:model="thumbnail" class="form-control mb-2">
+            @error('thumbnail') <p class="error mb-2">{{ $message }}</p> @enderror
+            @if($thumbnail)
+                <img src="{{ $thumbnail->temporaryUrl() }}" alt="">
+            @endif
+
+            <button type="submit" class="btn btn-outline-primary btn-block">Guardar Foto</button>
+        </form>
+        </div>
+    </div>
 </div>
