e4fdea1890a076e188dfb801fb5056307e354617
Rony Quispe <oghmand@gmail.com>
2021-06-22 20:39:21 -0500

    Shopping cart

---
+++ app/Models/CartManager.php
@@ -0,0 +1,35 @@
+<?php 
+
+namespace App\Models;
+
+class CartManager 
+{
+    private $sessionName = "shopping_cart_id";
+    private $cart;
+
+    public function __construct()
+    {
+        $this->cart = $this->findOrCreate($this->findSession());
+    }
+
+    private function findOrCreate($cartId = null){
+        $cart = null;
+
+        if (is_null($cartId)) {
+            $cart = ShoppingCart::create();
+        } else {
+            $cart = ShoppingCart::find($cartId);
+
+            if (is_null($cart)) {
+                $cart = ShoppingCart::create();
+            }
+        }
+
+        request()->session()->put($this->sessionName,$cart->id);
+        return $cart;
+    }
+
+    private function findSession(){
+        return request()->session()->get($this->sessionName);
+    }
+}
