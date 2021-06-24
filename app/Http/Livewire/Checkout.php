c8571d96a54bb7d41a674a4b1d040b76421240ee
Rony Quispe <oghmand@gmail.com>
2021-06-23 21:15:52 -0500

    Listando productos del carrito

--- app/Http/Livewire/Checkout.php
+++ app/Http/Livewire/Checkout.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\CartManager;
 use Livewire\Component;
 
 class Checkout extends Component
 {
+    public $cart;
+
+    public function mount(CartManager $cartManager){
+        $this->cart = $cartManager->getCart();
+    }
     public function render()
     {
-        return view('livewire.checkout')->extends('layouts.app')->section('content');
+        return view('livewire.checkout',[
+            'products' => $this->cart->products
+        ])->extends('layouts.app')->section('content');
     }
 }
--- resources/views/livewire/checkout.blade.php
+++ resources/views/livewire/checkout.blade.php
@@ -1,3 +1,30 @@
 <div>
     <h2 class="text-center">Produtos del carrito</h2>
+    <div class="card">
+        <div class="card-body">
+            <table class="table text-center table-bordered">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Nombre</th>
+                        <th>Acción</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($products as $product)
+                        <tr>
+                            <td>{{ $product->id }}</td>
+                            <td>{{ $product->name }}</td>
+                            <td><button class="btn btn-danger"><i class="fa fa-trash"></i></button></td>
+                        </tr>
+                    @endforeach
+                        <tr>
+                            <td></td>
+                            <td class="font-weight-bold">Total</td>
+                            <td class="font-weight-bold">{{ $products->sum('price') }}</td>
+                        </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
